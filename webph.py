a3640ab81d3f62268d68e5d52f013630ea776112
curious-anu <anucosmos99@gmail.com>
2025-06-22 12:16:44 +0530

    Add files via upload

--- webph.py
+++ webph.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,6 @@ def home():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # use 5000 locally, override on Render
+    app.run(host="0.0.0.0", port=port)
